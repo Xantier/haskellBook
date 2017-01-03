31ac4737b271425948156627d48f767049630d8e
Jussi Hallila <jussi@hallila.com>
2017-01-03 21:25:09 +0000

    Product type stuff for chapter 11

--- src/chapter11/example.hs
+++ src/chapter11/example.hs
@@ -32,3 +32,35 @@ instance (Num a, TooMany a) => TooMany (a, a) where
 
 instance (Num a, TooMany a) => TooMany (a, a) where
   tooMany (n, s) = tooMany n && tooMany s
+
+data OperatingSystem = GnuPlusLinux
+                      | OpenBSDPlusNevermindJustBSDStill
+                      | Mac
+                      | Windows
+                      deriving (Eq, Show)
+
+data ProgrammingLanguage =  Haskell
+                            | Agda
+                            | Idris
+                            | PureScript
+                            deriving (Eq, Show)
+
+data Programmer = Programmer { os :: OperatingSystem
+                             , lang :: ProgrammingLanguage }
+                             deriving (Eq, Show)
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+    [ GnuPlusLinux
+    , OpenBSDPlusNevermindJustBSDStill
+    , Mac
+    , Windows
+    ]
+
+allLanguages :: [ProgrammingLanguage]
+allLanguages = [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [Programmer]
+allProgrammers = [Programmer {os = x, lang = y} | x <- allOperatingSystems, y <- allLanguages]
+
+testSize :: Bool
+testSize = length allProgrammers == (length allOperatingSystems) * (length allLanguages)
